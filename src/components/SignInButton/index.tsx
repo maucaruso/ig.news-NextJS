ac57d12edc5b5063f4f21b2bf28f5668ae3ea221
maurici3 <carusojr@outlook.com>
2022-04-04 20:52:19 -0300

    chore: github authenticantion

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,19 +1,22 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const {data: session} = useSession();
+  
+  console.log(session);
 
-  return isUserLoggedIn ? (
+  return session ? (
     <button type="button" className={styles.signInButton}>
       <FaGithub color="#04d361" />
-      Maurici Caruso
-      <FiX color="#737380" className={styles.closeIcon} />
+      {session.user.name}
+      <FiX color="#737380" className={styles.closeIcon} onClick={() => {signOut()}} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button type="button" className={styles.signInButton} onClick={() => signIn('github')}>
       <FaGithub color="#eba417" />
       Sign in with GitHub
     </button>
