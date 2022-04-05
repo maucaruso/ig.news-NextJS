515fc0ae2be8259dddec203188a2cf9ccc7eab01
maurici3 <carusojr@outlook.com>
2022-04-05 08:18:17 -0300

    chore: setting up faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from 'faunadb';
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -14,4 +18,39 @@ export default NextAuth({
       }
     })
   ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      const userEmail = user.email
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email: userEmail } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+
+        return true
+      } catch {
+        return false
+      }
+      
+    }
+  }
 })
